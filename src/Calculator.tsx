b15b28f55df70bd4205b417c1101ff10d002beb8
unknown <adrian_bon@wp.pl>
2023-08-24 14:17:31 +0200

    add operation

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,19 +1,94 @@
-import Display from './components/Display/Display'
+import {Display, OperationDisplay} from './components/Display/Display'
 import Button from './components/Button/Button'
 import { useState } from 'react'
 
 function Calculator() {
-  const [DisplayValue, setDisplayValue] = useState('')
-  const updateDisplay = (value: string) => setDisplayValue((v) => v + value);
+  const [operationDisplay, setOperationDisplay] = useState('')
+  const [mainDisplay, setMainDisplay] = useState('0')
+  const [lastSign, setLastSign] = useState('')
+  const [resetMainDisplay, setResetMainDisplay] = useState(true)
+  const [resetOperationDisplay, setResetOperationDisplay] = useState(false)
+  const updateDisplay = (value: string) => {
+    if (resetOperationDisplay) {
+      setOperationDisplay('')
+      setResetOperationDisplay(false)
+    }
+    switch(value){
+      case '.':
+        setMainDisplay((v) => v + '.')
+        setResetMainDisplay(false)
+        break;
+      case '-':
+        mainDisplay.charAt(0) === '-'?setMainDisplay((v) => v.substring(1)):setMainDisplay((v) => '-' + v)
+        setResetMainDisplay(false)
+        break;
+      default:
+        if (resetMainDisplay){
+          setMainDisplay(value)
+          setResetMainDisplay(false)
+        } else {
+          setMainDisplay((v) => value === '0' && v === '0'?v:v + value)
+        }
+        break;
+    }
+  };
+  const operations = (sign: string) => {
+    let a = Number.parseFloat(operationDisplay.slice(0,-1))
+    let b = Number.parseFloat(mainDisplay)
+    const calc = (a:number, b:number, o:string) =>{
+      switch(o){
+        case '+':
+          return a + b;
+        case '-':
+          return a - b;
+        case '*':
+          return a * b;
+        case '/':
+          return a / b;
+      }
+    }
+    if(resetMainDisplay) {
+      setOperationDisplay((v) => v.slice(0, -1) + sign)
+      setLastSign(sign)
+      return
+    }
+    if (sign === '=') {
+      if (lastSign){
+        setOperationDisplay(`${a} ${lastSign} ${b} ${sign}`)
+        setMainDisplay(String(calc(a,b,lastSign)))
+      } else {
+        setOperationDisplay(`${b} ${sign}`)
+      }
+      setLastSign('')
+      setResetMainDisplay(true)
+      setResetOperationDisplay(true)
+      return
+    }
+    if (lastSign !== '') {
+      setOperationDisplay(String(calc(a,b,lastSign)))
+      setMainDisplay(String(calc(a,b,lastSign)))
+    } else {
+      setOperationDisplay(mainDisplay)
+      setMainDisplay(mainDisplay)
+    }
+    setOperationDisplay((v) => v + sign)
+    setLastSign(sign)
+    setResetMainDisplay(true)
+  }
   return (
     <>
-      <Display>{Number.parseFloat(DisplayValue) || 0}</Display>
+      <OperationDisplay>{operationDisplay}</OperationDisplay>
+      <Display>{mainDisplay}</Display>
       <div className="buttons">
         {[...Array(10)].map((e, i) =>
           <Button className='number' key={i} onClick={() => updateDisplay(i.toString())}>{i}</Button>)
         }
-        <Button className='number' key={'toggleNegative'} onClick={() => console.log("toggle")}>+/-</Button>
+        <Button className='operation' key={'x'} onClick={() => operations("*")}>*</Button>
+        <Button className='operation' key={'-'} onClick={() => operations("-")}>-</Button>
+        <Button className='operation' key={'+'} onClick={() => operations("+")}>+</Button>
+        <Button className='number' key={'toggleNegative'} onClick={() => updateDisplay('-')}>+/-</Button>
         <Button className='number' key={'dot'} onClick={() => updateDisplay('.')}>.</Button>
+        <Button className='equal' key={'='} onClick={() => operations("=")}>=</Button>
       </div>
     </>
   )
--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,10 +1,17 @@
 interface Props {
-    children: number
+    children: string
+}
+const OperationDisplay = ({children}: Props) => {
+    return (
+        <div className="display operationDisplay">{children}</div>
+    )
 }
 const Display = ({children}: Props) => {
+    let dot = children.split('.')
+    let string = Number.parseFloat(dot[0]).toLocaleString("en-US").split(',').join(' ') + (dot.length > 1?('.' + dot[1]):'')
     return (
-        <div className="display">{children.toLocaleString("en-US").split(',').join(' ')}</div>
+        <div className="display">{string}</div>
     )
 }
 
-export default Display
+export { Display, OperationDisplay }
